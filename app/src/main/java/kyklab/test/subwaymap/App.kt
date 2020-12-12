bd49d83b1fdd9e531b00ba588d496e8b8425df86
lazyyq <kykint@naver.com>
2020-12-12 16:24:01 +0900

    Print logs for uncaught exceptions

--- app/src/main/java/kyklab/test/subwaymap/App.kt
+++ app/src/main/java/kyklab/test/subwaymap/App.kt
@@ -2,17 +2,34 @@ package kyklab.test.subwaymap
 
 import android.app.Application
 import android.content.Context
+import java.io.File
+import java.util.*
 
 class App : Application() {
+    private var defaultUncaughtExceptionHandler: Thread.UncaughtExceptionHandler? = null
+
     override fun onCreate() {
         super.onCreate()
 
         application = this
+        defaultUncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler()
+        Thread.setDefaultUncaughtExceptionHandler(ExceptionHandler())
     }
 
     companion object {
-        private var application: Application? = null
+        private lateinit var application: Application
         val context: Context
-            get() = application!!.applicationContext
+            get() = application.applicationContext
+    }
+
+    private inner class ExceptionHandler : Thread.UncaughtExceptionHandler {
+        override fun uncaughtException(t: Thread, e: Throwable) {
+            val output: String =
+                getExternalFilesDir(null).toString() + File.separator + Date() + ".txt"
+            File(output).printWriter().use { e.printStackTrace(it) }
+
+            defaultUncaughtExceptionHandler?.uncaughtException(t, e)
+        }
     }
 }
+
